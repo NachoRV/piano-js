59eea3289b459ceb5ce9ce71bb7f3e46a2ffee57
irvb <irvb@MacBook-Pro-de-irvb.local>
2022-04-03 15:24:02 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// const btnDO = document.getElementById('btnDO');
+
+// btnDO.addEventListener('click', function(){
+//     const audio = document.getElementById('audioDO');
+//     audio.pause();
+//     audio.currentTime = 0;
+//     audio.play();
+// })
+
+// const btnRE = document.getElementById('btnRE');
+
+// btnRE.addEventListener('click', function(){
+//     const audio = document.getElementById('audioRE');
+//     audio.pause();
+//     audio.currentTime = 0;
+//     audio.play();
+// })
+
+const btns = document.querySelectorAll('button');
+
+btns.forEach( btn => {
+    btn.addEventListener('click', playSound);
+})
+
+function playSound(event) {
+    const nota = event.target.dataset.nota
+    const audio = document.getElementById('audio' + nota);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+}
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    console.log(key)
+    const btn = document.querySelector(`button[data-key="${key}"]`)
+
+})
